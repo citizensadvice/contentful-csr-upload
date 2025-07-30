ec060099c69fccefc3b0f0f308afb840b9d5781f
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-07-30 10:06:16 +0100

    feat: move to new react linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactPlugin from "eslint-plugin-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,10 +23,13 @@ export default defineConfig([
     "**/test-results",
   ]),
   {
-    extends: fixupConfigRules(compat.extends("react-app", "prettier")),
-
+    extends: fixupConfigRules(compat.extends("prettier")),
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    ...reactPlugin.configs.flat.recommended,
     rules: {
-      "no-unused-vars": ["error"],
+      "react/prop-types": "off",
     },
   },
 ]);
